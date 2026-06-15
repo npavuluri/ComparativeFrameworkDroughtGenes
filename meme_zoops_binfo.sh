374e3114f54d6fd88f4d768e46cb5d47e0139572
Namratha_Pavuluri <147601548+npavuluri@users.noreply.github.com>
2026-06-15 17:07:34 +0200

    Add files via upload

---
+++ meme_zoops_binfo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -J meme
+#SBATCH -o ./%x.%j.%N.out
+#SBATCH -D ./
+#SBATCH -e ./%x.%j.%N.err
+#SBATCH --get-user-env
+#SBATCH --partition=CPU
+#SBATCH --mem=50G
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+
+set -uo pipefail # leave out -e so you can see failures, all the jobs were failing before even writing to error file.
+IFS=$'\n\t'
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate meme || { echo "Conda failed to activate" >&2; exit 1; } #sbatch job fails before even activating the env, so to see what causes it.
+
+meme ./NAC_combined.fa -protein -mod zoops -nmotifs 20 -minw 10 -maxw 50 -evt 0.000001 -oc ./zoops/ -maxsize 1000000 -minsites 800 -searchsize 0 -brief 100000 #-brief flag gives the full output and using fimo is not necessary anymore. #-searchsize flag is necessary because meme doesn't find the motifs in all your sequences but rather samples a set which could be < 500 and then uses fimo to find the already discovered motifs in remaining sequences.
+
+
+# Always deactivate safely
+set +u
+conda deactivate
+set -u
+
